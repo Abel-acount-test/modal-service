5f87dcb52ff8a2998c16221cb18f9b85505ab1f1
davidillanis <brayanillanis@gmail.com>
2024-04-08 21:20:00 -0500

    basic proyect

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestModalComponent } from './modal/test-modal/test-modal.component';
+import { ModalTestComponent } from './component/modal-test/modal-test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestModalComponent
+    TestModalComponent,
+    ModalTestComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/component/modal-test/modal-test.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-test',
+  templateUrl: './modal-test.component.html',
+  styleUrls: ['./modal-test.component.css']
+})
+export class ModalTestComponent {
+
+}
