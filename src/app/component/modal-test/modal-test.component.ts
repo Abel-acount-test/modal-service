5215e5d243e18247ec8b196bbfdad40a23d536cf
davidillanis <brayanillanis@gmail.com>
2024-04-09 07:39:42 -0500

    existoso

--- src/app/component/modal-test/modal-test.component.ts
+++ src/app/component/modal-test/modal-test.component.ts
@@ -1,4 +1,5 @@
 import { Component, Injectable } from '@angular/core';
+import * as bootstrap from 'bootstrap';
 
 @Component({
   selector: 'app-modal-test',
@@ -9,10 +10,35 @@ import { Component, Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ModalTestComponent {
-  protected viewModal1():void{
+  protected viewModal1(): void {
+    const modalElement = document.getElementById('modal1');
+
+    if (modalElement) {
+      const modal = new bootstrap.Modal(modalElement);
+      modal.show();
+    } else {
+      console.error("No se encontró ningún elemento con el ID 'exampleModal'");
+    }
+
   }
-  protected viewModal2():void{
+  protected viewModal2(): void {
+    const modalElement = document.getElementById('modal2');
+
+    if (modalElement) {
+      const modal = new bootstrap.Modal(modalElement);
+      modal.show();
+    } else {
+      console.error("No se encontró ningún elemento con el ID 'exampleModal'");
+    }
   }
-  protected viewModal3():void{
+  protected viewModal3(): void {
+    const modalElement = document.getElementById('modal3');
+
+    if (modalElement) {
+      const modal = new bootstrap.Modal(modalElement);
+      modal.show();
+    } else {
+      console.error("No se encontró ningún elemento con el ID 'exampleModal'");
+    }
   }
 }
