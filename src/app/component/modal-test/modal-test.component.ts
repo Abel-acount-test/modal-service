ee5cc24c82b899b277f5d511193867d67d84d85e
davidillanis <brayanillanis@gmail.com>
2024-04-09 07:24:37 -0500

    view modal service 1

--- src/app/component/modal-test/modal-test.component.ts
+++ src/app/component/modal-test/modal-test.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
 
 @Component({
   selector: 'app-modal-test',
   templateUrl: './modal-test.component.html',
   styleUrls: ['./modal-test.component.css']
 })
+@Injectable({
+  providedIn: 'root'
+})
 export class ModalTestComponent {
-
+  protected viewModal1():void{
+  }
+  protected viewModal2():void{
+  }
+  protected viewModal3():void{
+  }
 }
